90132e7b76321179358e6ee525ff38ca06ca95a1
aksmf1442 <aksmf1442@naver.com>
2022-12-15 15:03:12 +0900

    test(자판기): 입력 받은 금액이 숫자가 아니라면 IllegalException을 발생시키는 기능 테스트 구현

--- src/main/java/vendingmachine/view/InputViewValidator.java
+++ src/main/java/vendingmachine/view/InputViewValidator.java
@@ -4,6 +4,9 @@ public class InputViewValidator {
 
     private static int MIN_DIVISION_NUMBER = 10;
 
+    public static void validateNumber(String money) {
+    }
+
     public static void validateDivisibleBy10(int money) {
         if (money % MIN_DIVISION_NUMBER != 0) {
             throw new IllegalArgumentException("[ERROR] 금액이 10으로 나누어 떨어져야 합니다.");
--- src/test/java/vendingmachine/view/InputViewValidatorTest.java
+++ src/test/java/vendingmachine/view/InputViewValidatorTest.java
@@ -7,6 +7,13 @@ import org.junit.jupiter.api.Test;
 class InputViewValidatorTest {
 
     @Test
+    void 금액이_숫자가_아니라면_IllegalArgumentException_발생() {
+        String money = "aa";
+        assertThatThrownBy(() -> InputViewValidator.validateNumber(money)).isInstanceOf(
+            IllegalArgumentException.class);
+    }
+
+    @Test
     void 금액이_10으로_나누어_떨어지지_않는다면_IllegalArgumentException_발생() {
         int money = 11;
         assertThatThrownBy(() -> InputViewValidator.validateDivisibleBy10(money))
