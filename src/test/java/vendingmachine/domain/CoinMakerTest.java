b5294d1ec55e8dfdf14dcb8503506cb24fbe2490
aksmf1442 <aksmf1442@naver.com>
2022-12-15 15:35:28 +0900

    test(자판기): 랜덤으로 생성된 코인들이 금액과 일치하는지 확인하는 기능 테스트 구현

---
+++ src/test/java/vendingmachine/domain/CoinMakerTest.java
@@ -0,0 +1,23 @@
+package vendingmachine.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+class CoinMakerTest {
+
+    @Test
+    void 랜덤으로_생성된_코인들이_금액과_일치하는지_확인() {
+        CoinMaker coinMaker = new CoinMaker();
+        int money = 450;
+
+        Map<Coin, Integer> coins = coinMaker.createCoins(money);
+        int result = 0;
+        for (Coin coin: coins.keySet()) {
+            result += coins.get(coin) * coin.getAmount();
+        }
+
+        assertThat(result).isEqualTo(money);
+    }
+}
